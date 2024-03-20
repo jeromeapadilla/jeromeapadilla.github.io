458714b0b6fed136bb27802d630a879bce674bfe
jeromeapadilla <jeromezkii888@gmail.com>
2024-03-19 21:16:29 -0400

    app

--- Portfolio/site/App.js
+++ Portfolio/site/App.js
@@ -1,7 +1,7 @@
 // App.js
 import React from 'react';
 import MovieCategory from './MovieCategory';
-import 'App.js';
+import './App.css'; // Corrected import statement
 
 function App() {
   return (
